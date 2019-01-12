639d4c4d3d2206a12fbf6a2e0351cd3bb1d22976
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2019-01-12 19:18:54 +1100

    add typescript and tslint

--- App.js
+++ App.js
@@ -1,67 +1,65 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Image, Platform, StyleSheet, Text, View} from 'react-native';
-import RNFetchBlob from 'rn-fetch-blob'
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importDefault(require("react"));
+var react_2 = require("react");
+var react_native_1 = require("react-native");
+var dg_logo_png_1 = __importDefault(require("./assets/images/dg-logo.png"));
+var instructions = react_native_1.Platform.select({
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
 });
-
-type Props = {};
-export default class App extends Component<Props> {
-  download = () => {
-    let dirs = RNFetchBlob.fs.dirs
-    RNFetchBlob
-    .config({
-      // response data will be saved to this path if it has access right.
-      path : dirs.DownloadDir + '/download-example.png'
-    })
-    .fetch('GET', 'https://raw.githubusercontent.com/joltup/rn-fetch-blob/master/img/RNFB-Flow-hd.png', {
-      //some headers ..
-    })
-    .then((res) => {
-      // the path should be dirs.DocumentDir + 'path-to-file.anything'
-      console.log('The file saved to ', res.path())
-    })
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Image source={require('./assets/images/dg-logo.png')} />
-        <Text onPress={this.download} style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>YES To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+var App = /** @class */ (function (_super) {
+    __extends(App, _super);
+    function App() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.state = {
+            text: 'Something'
+        };
+        return _this;
+    }
+    App.prototype.render = function () {
+        var _this = this;
+        return (react_1.default.createElement(react_native_1.View, { style: styles.container },
+            react_1.default.createElement(react_native_1.Image, { source: dg_logo_png_1.default, style: { width: 70, height: 50 } }),
+            react_1.default.createElement(react_native_1.Text, { onPress: function () { return console.log('BLAH'); }, style: styles.instructions }, "YES To get started, edit App.js"),
+            react_1.default.createElement(react_native_1.Text, { onPress: function () { return _this.setState({ text: _this.state.text + 'yes' }); }, style: styles.instructions }, this.state.text),
+            react_1.default.createElement(react_native_1.Text, { style: styles.instructions }, instructions)));
+    };
+    return App;
+}(react_2.Component));
+exports.default = App;
+var styles = react_native_1.StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
---
+++ dist/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Component } from 'react';
+import { Image, Platform, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/images/dg-logo.png';
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
+});
+export default class App extends Component {
+    constructor() {
+        super(...arguments);
+        this.state = {
+            text: 'Something'
+        };
+    }
+    render() {
+        return (<View style={styles.container}>
+        <Image source={logo} style={{ width: 70, height: 50 }}/>
+        <Text onPress={() => console.log('BLAH')} style={styles.instructions}>YES To get started, edit App.js</Text>
+        <Text onPress={() => this.setState({ text: this.state.text + 'yes' })} style={styles.instructions}>{this.state.text}</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>);
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+//# sourceMappingURL=App.js.map
