5285e6dbd58284fce088227c4765e357248f9878
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2019-07-08 10:43:25 +1000

    build

--- dist/App.js
+++ dist/App.js
@@ -1,10 +1,16 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+import About from '~src/containers/About/About';
+import Contact from '~src/containers/Contact/Contact';
+import Gallery from '~src/containers/Gallery/Gallery';
 import Home from '~src/containers/Home/Home';
-import Illustration from '~src/containers/Illustration/Illustration';
+import Illustrations from '~src/containers/Illustrations/Illustrations';
 import Project from '~src/containers/Project/Project';
 const AppNavigator = createStackNavigator({
+    About,
+    Contact,
+    Gallery,
     Home,
-    Illustration,
+    Illustrations,
     Project
 }, {
     headerMode: 'none',
